0b0db24247c712064f2b47a9b925b42815396a33
LAPTOP-TT69N9TF\wjc <214095883@qq.com>
2018-07-06 17:02:23 +0800

    1、优化代码

--- ibe-api/src/test/java/com/sparrow/ibe/bookingservice/airbook/AirBookTests.java
+++ ibe-api/src/test/java/com/sparrow/ibe/bookingservice/airbook/AirBookTests.java
@@ -1,6 +1,17 @@
 package com.sparrow.ibe.bookingservice.airbook;
 
 import com.sparrow.BaseTests;
+import com.sparrow.app.config.IBEConfig;
+import com.sparrow.ibe.bookingservice.airbook.dao.AirBookDao;
+import com.sparrow.ibe.bookingservice.airbook.model.AirBookRequest;
+import com.sparrow.ibe.bookingservice.airbook.model.AirBookResponse;
+import com.sparrow.ibe.bookingservice.airbook.model.AirReservation;
+import com.sparrow.ibe.model.DefaultError;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * 接口类别：预订服务
@@ -12,4 +23,34 @@ import com.sparrow.BaseTests;
  * @create 2018/7/4
  */
 public class AirBookTests extends BaseTests {
+
+    @Autowired
+    private IBEConfig ibeConfig;
+
+    /**
+     * 测试场景：国内航班+单个成人+单程
+     */
+    @Test
+    public void testAirBook1() {
+        int type = 1;
+        AirBookRequest request = new AirBookRequestBuilder().buildRequest(type);
+        AirBookDao dao = new AirBookDao();
+        AirBookResponse response = (AirBookResponse) dao.execute(ibeConfig.getUsername(), ibeConfig.getPassword(), request);
+
+        Assert.assertNotNull(response);
+
+        logger.info("response.getAirReservationList().size(): {}", response.getAirReservationList().size());
+
+        List<AirReservation> airReservationList = response.getAirReservationList();
+        for (AirReservation ar : airReservationList) {
+            logger.info("CommentList:" + ar.getCommentList());
+        }
+        List<DefaultError> errorList = response.getErrorList();
+        logger.info("errorList.size(): {}", errorList.size());
+        for (DefaultError error : errorList) {
+            logger.info("errorCode: {}", error.getCode());
+            logger.info("error.getCnMessage(): {}", error.getCnMessage());
+        }
+    }
+
 }
