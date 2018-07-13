1edcf429e993bfd2e2a98cb2f4969c53054ac67c
LAPTOP-TT69N9TF\wjc <214095883@qq.com>
2018-07-13 22:54:08 +0800

    1、删除ieanalysis项目和ieanalysis-transform项目

--- ieanalysis/src/main/resources/static/js/public/query-province.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$(".queryProvinceSelect").find("li:first").find("a").addClass("changeColor");
-	selectProvince=$(".queryProvinceSelect").find("li:first").find("a").text();
-	//console.log(selectProvince);
-});
- 
