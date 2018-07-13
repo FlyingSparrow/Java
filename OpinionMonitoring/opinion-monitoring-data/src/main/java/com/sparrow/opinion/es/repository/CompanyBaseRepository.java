de219b818994802177cd0a5c7fa6aa52bcc12f6c
LAPTOP-TT69N9TF\wjc <214095883@qq.com>
2018-07-13 22:16:09 +0800

    1、删除OpinionMonitoring项目

--- OpinionMonitoring/opinion-monitoring-data/src/main/java/com/sparrow/opinion/es/repository/CompanyBaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparrow.opinion.es.repository;
-
-
-import com.sparrow.opinion.es.entity.EsCompanyBase;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CompanyBaseRepository extends ElasticsearchRepository<EsCompanyBase, String> {
-
-}
--- OpinionMonitoring/opinion-monitoring-data/src/main/java/com/sparrow/opinion/es/repository/CompanyInvestorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparrow.opinion.es.repository;
-
-import com.sparrow.opinion.es.entity.EsCompanyInvestor;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CompanyInvestorRepository extends ElasticsearchRepository<EsCompanyInvestor, String> {
-
-}
