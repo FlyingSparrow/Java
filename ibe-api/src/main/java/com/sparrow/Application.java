4c36c213289ef867465f1b48819dd107fd9c179d
LAPTOP-TT69N9TF\wjc <214095883@qq.com>
2018-12-03 17:41:12 +0800

    1、添加tomcat部署支持

--- ibe-api/src/main/java/com/sparrow/Application.java
+++ ibe-api/src/main/java/com/sparrow/Application.java
@@ -2,6 +2,8 @@ package com.sparrow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,9 +15,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableTransactionManagement
 @EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
